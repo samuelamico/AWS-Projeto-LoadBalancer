eb80c76c33015df838be56c05fdd0b99507a0148
samuelamico <sam.fst@gmail.com>
2020-05-26 08:51:10 -0300

    clean

--- Aplicacao/rds.conf.php
+++
@@ -1,1 +0,0 @@
-<?php $RDS_URL=''; $RDS_DB='banco'; $RDS_user='admin'; $RDS_pwd='SENHA'; ?>
--- Aplicacao/s3-conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-$accessKey = 'Access key ID';
-$secretKey = 'Secret access key';
-$region = 'us-east-1';
-$bucket = 'NOME DO BUCKET';
-$arqName =  'logo.jpg';
-?>
